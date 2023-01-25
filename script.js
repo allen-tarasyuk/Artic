0d12d119ca274250dacba3bc44a8d78e293ea96e
Allen Tarasyuk <allentarasyuk@allens-mbp.attlocal.net>
2023-01-24 22:58:42 -0800

    Adding files

---
+++ script.js
@@ -0,0 +1,49 @@
+
+ // Javascript Toggle Menu 
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+
+var swiper = new Swiper(".slide-content", {
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteration: false,
+  },
+
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 4,
+      },
+  },
+});
